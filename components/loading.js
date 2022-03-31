648cbdf17b5756716f6fe9aa8eb2291922e326dc
Mastrayasa <mastrayasa@gmail.com>
2022-03-31 18:09:07 +0800

    ok

--- components/loading.js
+++ components/loading.js
@@ -1,10 +1,9 @@
-import {MdDataUsage} from 'react-icons/md'
 export default function Loading(props){
 
     const size = props.size ? props.size : 'text-xl'
 
     return (<span className="">
-        <MdDataUsage className={`animate-spin inline ${size} -mt-1 mr-1`} />
+         
         <span className="">
             {props.text ? props.text : 'Loading...'}
         </span>
